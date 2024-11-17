d3af2439efbf3c01cd27e4be67794ce1120da36f
João Santos <87877655+jvdss@users.noreply.github.com>
2024-11-17 08:12:34 -0300

    Add files via upload

---
+++ k.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define FOR(i, n) for(int i = 0; i < (int) n; i++)
+#define rall(x) x.rbegin(),x.rend()
+#define all(x) x.begin(),x.end()
+#define pb push_back
+#define ss second
+#define ff first
+#define endl '\n'
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+ 
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+const int INF = 0x3f3f3f3f;
+const int MAX = 2e5+7;
+
+void solve(){
+
+} // check overflow and sizes
+ 
+int32_t main(){
+    ios::sync_with_stdio(false); cin.tie(0);
+    int var = 1;
+    // cin >> var;
+    for(; var; --var) solve();
+    return 0;
+}
